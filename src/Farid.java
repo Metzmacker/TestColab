2caffae4ca9ff7c33160f814eb8b480335020933
id-farid <farid.idbourhim@hotmail.com>
2016-01-23 18:30:48 +0100

    nouvelle mofification

--- src/Farid.java
+++ src/Farid.java
@@ -4,6 +4,7 @@ public class Farid {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Salut a vous tous...");
+		System.out.println("ca va...");
 	}
 
 }
